6c9b0e1424c24ae66d3b1dc5d9076d96f93ac0fd
RonaldNestor <ronald4261@hotmail.com>
2015-04-13 13:49:09 -0500

    result of search

--- catalogo/views.py
+++ catalogo/views.py
@@ -4,17 +4,22 @@ from django.contrib.auth import login, logout, authenticate
 from django.views.generic import TemplateView
 from .models import Item
 # Create your views here.
-def home(request):    
+def home(request):
     return render_to_response('home.html',context_instance=RequestContext(request))
 
 def logout_view(request):
     logout(request)
 
-class SearchView(TemplateView):	
-	#template_name = "buscar.html"
-	def post(self, request, *args, **kwargs):
-		s = request.POST['fieldSearch']		
-		items = Item.objects.filter(title__contains=s)
-		print items
-		return render(request, 'buscar.html')
-		
+class SearchView(TemplateView):
+    def post(self, request, *args, **kwargs):
+        s = request.POST['fieldSearch']
+        items = Item.objects.filter(title__icontains=s)
+        # return render(request, 'buscar.html')
+        if items:
+            # data = []
+            # for item in items:
+            #     authors =item.author.all()
+            #     data.append(dict([(item,authors)]))
+            return render(request,'buscar.html',{'items':items,'s':s})
+        else:
+            return render(request,'buscar.html',{'s':s})
